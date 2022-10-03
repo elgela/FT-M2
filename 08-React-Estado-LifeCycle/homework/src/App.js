3db0d1677bcdc66a717c60ecb3c87b8913b7c011
Marcelo <gelatomarcelo@hotmail.com>
2022-10-03 17:44:58 -0300

    inc

--- 08-React-Estado-LifeCycle/homework/src/App.js
+++ 08-React-Estado-LifeCycle/homework/src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './App.css';
 import Nav from './components/Nav.jsx';
-import Cards from './components/Cards.jsx';
+// import Cards from './components/Cards.jsx';
 
 
 export default function App() {
+  
+  function onSearch(ciudad) {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}&units=metric`)
+  }
+  
   return (
     <div className="App">
-      <Nav/>
+      <Nav onSearch={onSearch}/>
       <h1>Título</h1>
     </div>
   );
--- 08-React-Estado-LifeCycle/homework/src/components/SearchBar.jsx
+++ 08-React-Estado-LifeCycle/homework/src/components/SearchBar.jsx
@@ -1,14 +1,22 @@
 import React, { useState } from "react";
 
 export default function SearchBar({onSearch}) {
+  const [city, setCity] = useState('');
+  
+  function handleInputChange(e) {
+    setCity(e.target.value)
+  }
+  
   return (
     <form onSubmit={(e) => {
       e.preventDefault();
-      onSearch("Cairns");
+      onSearch(city);
     }}>
       <input
         type="text"
         placeholder="Ciudad..."
+        value={city} //ROMA
+        onChange={handleInputChange}
       />
       <input type="submit" value="Agregar" />
     </form>
