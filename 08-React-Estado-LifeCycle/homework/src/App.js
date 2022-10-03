604352105f3eaefae43ad18e1e5af53b0c3bb88b
elgela <gelatomarcelo@hotmail.com>
2022-10-03 18:44:07 -0300

    inc

--- 08-React-Estado-LifeCycle/homework/src/App.js
+++ 08-React-Estado-LifeCycle/homework/src/App.js
@@ -1,19 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Cards from './components/Cards';
 import Nav from './components/Nav.jsx';
-// import Cards from './components/Cards.jsx';
 
+// const apiKey = process.env.REACT_APP_WEATHER_APP;
+const apiKey = '8f995cbba8e7825d6c1a11137c7013a3';
 
 export default function App() {
+  const [cities, setCities] = useState([]);
   
   function onSearch(ciudad) {
     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}&units=metric`)
+    .then(res => res.json())
+    .then(recurso => {
+      if(recurso.main !== undefined){
+        const ciudad = {
+          min: Math.round(recurso.main.temp_min),
+          max: Math.round(recurso.main.temp_max),
+          img: recurso.weather[0].icon,
+          id: recurso.id,
+          wind: recurso.wind.speed,
+          temp: recurso.main.temp,
+          name: recurso.name,
+          weather: recurso.weather[0].main,
+          clouds: recurso.clouds.all,
+          latitud: recurso.coord.lat,
+          longitud: recurso.coord.lon
+        };
+        setCities(oldCities => [...oldCities, ciudad]);
+      } else {
+        alert("Ciudad no encontrada");
+      }
+    })
   }
   
   return (
     <div className="App">
       <Nav onSearch={onSearch}/>
-      <h1>Título</h1>
+      <Cards cities={cities}/>
     </div>
   );
 }
