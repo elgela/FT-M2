e7eb8974cf54825c99d6f2618a443f2628929fd4
elgela <gelatomarcelo@hotmail.com>
2022-10-10 11:16:28 -0300

    hcompletando

--- 10-React-Forms/homework/src/Form.jsx
+++ 10-React-Forms/homework/src/Form.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
 export default function  Form() {
+  const [username, setUsername] = React.useState('')
+  const [password, setPassword] = React.useState('')
   return (
+    <form>
       <div>
-        Componente Form
+        <label>Username:</label>
+        <input type='text' name='username' value={username} onChange={(e) => setUsername(e.target.value)} valor={username}/>
       </div>
+      <div>
+        <label>Password:</label>
+        <input type='text' name='password' value={password} onChange={(e) => setPassword(e.target.value)} valor={password}/>
+      </div>
+    </form>
   )
 }
